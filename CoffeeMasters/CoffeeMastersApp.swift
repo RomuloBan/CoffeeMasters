070258916f5bf37d63ba341b82f3252b5ce7a3bc
Romulo Banares <romulo.banares.contractor@bbva.com>
2022-08-02 13:57:45 +0200

    Initial Commit

---
+++ CoffeeMasters/CoffeeMastersApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeMastersApp.swift
+//  CoffeeMasters
+//
+//  Created by ROMULO BAÑARES ROCA on 2/8/22.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeMastersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
