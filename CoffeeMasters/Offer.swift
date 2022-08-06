a4cd217cb459367285bf5f5d81a71163f6b665f9
Romulo Banares <romulo.banares.contractor@bbva.com>
2022-08-06 23:33:37 +0200

    add background images

--- CoffeeMasters/Offer.swift
+++ CoffeeMasters/Offer.swift
@@ -12,9 +12,19 @@ struct Offer: View {
     var description = ""
     
     var body: some View {
-        VStack {
-            Text(title)
-            Text(description)
+        ZStack {
+            Image("BackgroundPattern")
+                .frame(maxWidth: .infinity, maxHeight: 200)
+                .clipped()
+            VStack {
+                Text(title)
+                    .padding()
+                    .font(.title)
+                    .background(Color("CardBackground"))
+                Text(description)
+                    .padding()
+                    .background(Color("CardBackground"))
+            }
         }
     }
 }
@@ -23,6 +33,7 @@ struct Offer_Previews: PreviewProvider {
     static var previews: some View {
         Group {
             Offer(title: "My offer", description: "This is a description.")
+                .previewLayout(.fixed(width: 350, height: 200))
         }
     }
 }
---
+++ CoffeeMasters/OffersPage.swift
@@ -0,0 +1,25 @@
+//
+//  OffersPage.swift
+//  CoffeeMasters
+//
+//  Created by ROMULO BAÑARES ROCA on 6/8/22.
+//
+
+import SwiftUI
+
+struct OffersPage: View {
+    var body: some View {
+        VStack {
+            Offer(title: "Early Coffee",
+                description: "10% off. Offer valid from 6am to 9am")
+            Offer(title: "Welcome Gift",
+                description: "25% off on your first order")
+        }
+    }
+}
+
+struct OffersPage_Previews: PreviewProvider {
+    static var previews: some View {
+        OffersPage()
+    }
+}
