bc38c456caa8a48e598fcb040de3b8e3d7f0d0aa
Romulo Banares <romulo.banares.contractor@bbva.com>
2022-08-19 17:02:27 +0200

    add product

---
+++ CoffeeMasters/Model/Product.swift
@@ -0,0 +1,20 @@
+//
+//  Product.swift
+//  CoffeeMasters
+//
+//  Created by ROMULO BAÑARES ROCA on 19/8/22.
+//
+
+import Foundation
+
+struct Product {
+    var id: Int
+    var name: String
+    var description: String?
+    var price: Double
+    var image: String = ""
+    
+    var imageURL: URL {
+        URL(string: "https://firtman.github.io/coffeemasters/api/images/\(self.image)")!
+    }
+}
