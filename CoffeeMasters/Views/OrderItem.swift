2e22bed108c015820df936a1842b69b61468082b
Romulo Banares <romulo.banares.contractor@bbva.com>
2022-09-30 17:30:38 +0200

    add orderItem view

---
+++ CoffeeMasters/Views/OrderItem.swift
@@ -0,0 +1,20 @@
+//
+//  OrderItem.swift
+//  CoffeeMasters
+//
+//  Created by ROMULO BAÑARES ROCA on 23/8/22.
+//
+
+import SwiftUI
+
+struct OrderItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OrderItem_Previews: PreviewProvider {
+    static var previews: some View {
+        OrderItem()
+    }
+}
